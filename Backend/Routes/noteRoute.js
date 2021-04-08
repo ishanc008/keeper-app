03ea47e5c84c433a126fff989737935613f43b86
ishanc008 <ishanc008@gmail.com>
2021-04-08 20:11:32 +0530

    Added backend using express

---
+++ Backend/Routes/noteRoute.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+let Note = require("../Models/user_note")
+
+router.route("/").get((req,res)=>{
+    Note.find()
+       .then(notes => res.json(notes))
+       .catch(err => res.status(400).json("Error: "+err));
+})
+
+router.route("/add").post((req,res)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    const newNote = new Note({
+        title,
+        content
+    })
+    newNote.save()
+        .then(()=>res.json("Note added"))
+       .catch(err => res.status(400).json("Error: "+err));
+})
+
+module.exports = router;
